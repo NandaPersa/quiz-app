d97923df1151fad83b8584b4f6cb56016c7403de
NandaPersa <anandapersa@gmail.com>
2024-07-04 11:33:36 -0300

    fix: add navigate to home

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,13 +1,26 @@
+'use client';
+
 import ContainerComponent from '@/components/ui/Container';
 import styles from '@/styles/pages/notFound.module.scss';
 import { Button } from '@mui/material';
+import { useRouter } from 'next/navigation';
 
 function NotFoundPage() {
+  const router = useRouter();
+
+  function handleClick() {
+    router.push('/');
+  }
   return (
     <ContainerComponent>
       <h1 className={styles.notFound}>404</h1>;
       <p className={styles.text}>Página não encontrada.</p>
-      <Button type="submit" size="large" className={styles.button}>
+      <Button
+        type="submit"
+        size="large"
+        className={styles.button}
+        onClick={() => handleClick()}
+      >
         Retornar ao jogo
       </Button>
     </ContainerComponent>
