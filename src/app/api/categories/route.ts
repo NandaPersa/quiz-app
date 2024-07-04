b12ae2d921d7d492b07481a1e87663f1718e1c70
NandaPersa <anandapersa@gmail.com>
2024-07-03 23:09:06 -0300

    refactor: remove unnecessary console.log

--- src/app/api/categories/route.ts
+++ src/app/api/categories/route.ts
@@ -4,7 +4,6 @@ export const GET = async () => {
   try {
     const response = await fetch(`${process.env.API_URL}/categories`);
     const data = await response.json();
-    console.log(data);
     return NextResponse.json(data);
   } catch (error) {
     return NextResponse.json({}, { status: 500 });
